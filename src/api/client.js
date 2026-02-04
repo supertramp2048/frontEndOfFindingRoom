2fc679c1f8f949dfe199e73a7d092d88bc454f28
ahay2048 <phamhuy27082004@gmail.com>
2026-02-04 11:13:07 +0700

    fix

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = import.meta.process.env.API_BASE_URL
 
 const client = axios.create({
   baseURL: API_BASE_URL,
