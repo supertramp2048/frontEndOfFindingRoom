4ba6cf70df58e27760383a9e3f5afbcb068da261
ahay2048 <phamhuy27082004@gmail.com>
2026-02-04 11:15:56 +0700

    fix second

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.process.env.API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_BASE_URL
 
 const client = axios.create({
   baseURL: API_BASE_URL,
